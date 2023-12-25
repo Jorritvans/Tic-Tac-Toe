b40ff263aaf943e54c10d8fac03222aad7174613
Jorritvans <>
2023-12-25 14:25:17 +0000

    adding the first function which is returning the class that will be put in so if O class is used it will print and O if not it will print X

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 const X_CLASS = 'X'
 const O_CLASS = 'O'
 const cellElements = document.querySelectorAll('[data-cell]')
+let OTurn
 
 cellElements.forEach(cell => {
     cell.addEventListener('click', handleClick, {once: true})
 })
 
 function handleClick(e) {
-    
+    const cell = e.target
+    const currentClass = OTurn ? O_CLASS : X_CLASS
 }
