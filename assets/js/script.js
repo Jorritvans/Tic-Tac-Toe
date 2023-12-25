1765738b4194656d865165930144c492a05f080a
Jorritvans <>
2023-12-25 15:00:27 +0000

    adding a gamestart function so you can start te game with the X showing up in a hover status

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,25 @@
 const X_CLASS = 'X'
 const O_CLASS = 'O'
 const cellElements = document.querySelectorAll('[data-cell]')
+const gameBoard = document.getElementById('gameboard')
 let OTurn
 
-cellElements.forEach(cell => {
-    cell.addEventListener('click', handleClick, {once: true})
-})
+startGame()
+
+function startGame () {
+    OTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true });
+    })
+    setGameBoardHoverClass()
+}
 
 function handleClick(e) {
     const cell = e.target
     const currentClass = OTurn ? O_CLASS : X_CLASS
     placeMark(cell, currentClass)
     swapTurns()
+    setGameBoardHoverClass()
 }
 
 function placeMark(cell, currentClass) {
@@ -20,4 +28,14 @@ function placeMark(cell, currentClass) {
 
 function swapTurns() {
     OTurn = !OTurn 
+}
+
+function setGameBoardHoverClass() {
+    gameBoard.classList.remove(X_CLASS)
+    gameBoard.classList.remove(O_CLASS)
+    if (OTurn) {
+        gameBoard.classlist.add(O_CLASS)
+    } else {
+        gameBoard.classList.add(X_CLASS)
+    }
 }
