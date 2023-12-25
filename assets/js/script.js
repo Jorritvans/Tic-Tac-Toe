0382e8783e4ccbe1ed81285051cbdd1b002a3ffb
Jorritvans <>
2023-12-25 17:11:53 +0000

    not finding a fix leaving it for tomorrow.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,90 @@
-const X_CLASS = 'X'
-const O_CLASS = 'O'
+const X_CLASS = 'X';
+const O_CLASS = 'O';
 const WINNING_COMBINATIONS = [
-   [0, 1, 2],
-   [3, 4, 5],
-   [6, 7, 8],
-   [0, 3, 6],
-   [1, 4, 7],
-   [2, 5, 8],
-   [0, 4, 8],
-   [2, 4, 6]
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.getElementById('gameboard')
-const winningMessageElement = document.getElementById('winningMessage')
-const resetButton = document.getElementById('resetButton')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
-let OTurn
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('gameboard');
+const winningMessageElement = document.getElementById('winningMessage');
+const restartButton = document.getElementById('restartButton');
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]');
+let OTurn;
 
-startGame()
-resetButton.addEventListener('click', startGame)
+startGame();
 
-function startGame () {
-    OTurn = false
+restartButton.addEventListener('click', startGame);
+
+function startGame() {
+    OTurn = false;
     cellElements.forEach(cell => {
         cell.classList.remove(X_CLASS);
         cell.classList.remove(O_CLASS);
         cell.removeEventListener('click', handleClick);
         cell.addEventListener('click', handleClick, { once: true });
-        setBoardHoverClass()
-        winningMessageElement.classList.remove('show')
-    })  
+    });
+    setBoardHoverClass();
+    winningMessageElement.classList.remove('show');
 }
 
 function handleClick(e) {
-    const cell = e.target
-    const currentClass = OTurn ? O_CLASS : X_CLASS
-    placeMark(cell, currentClass)
+    const cell = e.target;
+    const currentClass = OTurn ? O_CLASS : X_CLASS;
+    placeMark(cell, currentClass);
     if (checkWin(currentClass)) {
-        endGame(false)
+        endGame(false);
     } else if (isDraw()) {
-        endGame(true)
+        endGame(true);
     } else {
-    swapTurns()
-    setBoardHoverClass()
+        swapTurns();
+        setBoardHoverClass();
     }
 }
 
 function endGame(draw) {
     if (draw) {
-        winningMessageTextElement.innerText = 'Draw!'
+        winningMessageTextElement.innerText = 'Draw!';
     } else {
-        winningMessageTextElement.innerText = `${OTurn ? "O's" : "X's"} Wins!`
+        winningMessageTextElement.innerText = `${OTurn ? "O's" : "X's"} Wins!`;
     }
-    winningMessageTextElement.classList.add('show')
+    winningMessageTextElement.classList.add('show');
 }
 
 function isDraw() {
     return [...cellElements].every(cell => {
         return cell.classList.contains(X_CLASS) ||
-        cell.classList.contains(O_CLASS)
-    })
+            cell.classList.contains(O_CLASS);
+    });
 }
 
 function placeMark(cell, currentClass) {
-    cell.classList.add(currentClass)
+    cell.classList.add(currentClass);
 }
 
 function swapTurns() {
-    OTurn = !OTurn 
+    OTurn = !OTurn;
 }
 
 function setBoardHoverClass() {
-    board.classList.remove(X_CLASS)
-    board.classList.remove(O_CLASS)
+    board.classList.remove(X_CLASS);
+    board.classList.remove(O_CLASS);
     if (OTurn) {
-        board.classList.add(O_CLASS)
+        board.classList.add(O_CLASS);
     } else {
-        board.classList.add(X_CLASS)
+        board.classList.add(X_CLASS);
     }
 }
 
 function checkWin(currentClass) {
     return WINNING_COMBINATIONS.some(combination => {
         return combination.every(index => {
-            return cellElements[index].classList.contains(currentClass)
-        })
-    })
+            return cellElements[index].classList.contains(currentClass);
+        });
+    });
 }
